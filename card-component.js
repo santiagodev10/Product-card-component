56ab027c5a692f34405c0ba87e92098ff663fb71
santiagodev <sanxp18@gmail.com>
2025-07-15 12:56:04 -0400

    add styles for mobile version

--- card-component.js
+++ card-component.js
@@ -16,112 +16,123 @@ class CardComponent extends LitElement {
     static styles = css`
         /* Usa :host para aplicar estilos al elemento personalizado en sí */
         :host {
-            display: block; /* Los elementos personalizados son inline por defecto */
-            border: 1px solid #eee;
+            margin: 0;
+            padding: 0;
+            font-size: 62.5%; /* Base para rem */
+            box-sizing: border-box; /* Asegura que el padding y el border se incluyan en el ancho y alto total */
+            display: inline-block; /* Los elementos personalizados son inline por defecto */
             border-radius: 8px;
             overflow: hidden;
             box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-            max-width: 300px;
-            margin: 15px;
+            // max-width: 300px;
+            margin: 1rem;
             font-family: sans-serif;
         }
 
-        article {
-            display: flex;
-            flex-direction: column;
-            height: 100%;
+        article.card-component {
+            // display: flex;
+            // flex-direction: column;
+            // height: 100%;
+            display: grid;
+            grid-template-rows: 14rem 20rem;
+            max-width: 500px; 
         }
 
-        div:first-of-type { /* Contenedor para marca e imagen */
+        .card-component--brand-container { /* Contenedor para marca e imagen */
             position: relative;
             text-align: center;
             padding-bottom: 10px; /* Espacio para la marca */
-            background-color: #f9f9f9;
+            background-color: #5a6cb2;
         }
 
-        span { /* Nombre de la marca */
+        .brand-container--brand { /* Nombre de la marca */
             position: absolute;
-            top: 10px;
             left: 10px;
-            background-color: rgba(255, 255, 255, 0.8);
-            padding: 4px 8px;
             border-radius: 4px;
-            font-size: 0.8em;
-            font-weight: bold;
-            color: #555;
+            font-size: 100px;
+            font-weight: 800;
+            color: #5161a0;
             z-index: 1;
         }
 
-        figure {
-            margin: 0;
+        .brand-container--image-container {
+            position: absolute;
+            top: 5rem;
             width: 100%;
-            height: 200px; /* Altura fija para el contenedor de la imagen */
+            margin: 0 auto;
             overflow: hidden;
-            display: flex;
-            justify-content: center;
-            align-items: center;
         }
 
-        img {
-            max-width: 100%;
+        .brand-container--image-container img {
+            width: 100%;
+            max-width: 20rem;
             max-height: 100%;
+            margin: 0 auto;
             object-fit: contain; /* Asegura que la imagen se ajuste sin recortar */
-            display: block;
         }
 
-        div:last-of-type { /* Contenedor para los detalles del texto */
-            padding: 15px;
+        .card-component--details-container { /* Contenedor para los detalles del texto */
+            padding: 1rem;
             display: flex;
             flex-direction: column;
             flex-grow: 1; /* Permite que esta sección ocupe el espacio restante */
-            justify-content: space-between;
+            justify-content: space-around;
+            background-color: #f9f9f9;
         }
 
-        .product-title {
-            font-size: 1.3em;
-            margin: 0 0 5px 0;
-            color: #333;
+        .details-container--text-info {
+            margin-top: 1rem;
         }
 
-        .product-collection {
-            font-size: 0.9em;
-            color: #666;
-            margin: 0 0 10px 0;
+        .text-info--product-title {
+            display: inline;
+            padding: 0;
+            margin: 0;
+            margin-right: 1rem;
+            font-size: 1.8rem;
+            color: #444444;
         }
 
-        .product-description {
-            font-size: 0.85em;
-            color: #777;
-            line-height: 1.4;
-            margin: 0 0 15px 0;
-            flex-grow: 1; /* Permite que la descripción ocupe el espacio disponible */
+        .text-info--product-collection {
+            display: inline;
+            padding: 0;
+            margin: 0;
+            font-size: 1.2rem;
+            font-weight: 300;
+            color: #a69999;
+            letter-spacing: 0.1rem;
         }
 
-        div:last-of-type > div { /* Contenedor de precio y botón */
+        .details-container--product-description {
+            margin: 0;
+            font-size: 0.8rem;
+            color: #444444;
+        }
+
+        .details-container--price-info { /* Contenedor de precio y botón */
             display: flex;
             justify-content: space-between;
             align-items: center;
             margin-top: 10px; /* Espacio encima del precio/botón */
         }
 
-        .price {
-            font-size: 1.4em;
+        .price-info--price {
+            font-size: 1.6rem;
             font-weight: bold;
-            color: #007bff;
+            color: #a69999;
         }
 
-        button {
-            background-color: #007bff;
+        .price-info--price-button {
+            background-color: #5a6cb2;
             color: white;
-            border: none;
             padding: 8px 15px;
-            border-radius: 5px;
+            border-radius: 2rem;
             cursor: pointer;
-            font-size: 1em;
+            font-size: 1rem;
             transition: background-color 0.2s ease;
         }
 
-        button:hover {
+        .price-info--price-button:hover {
             background-color: #0056b3;
         }
     `;
@@ -131,19 +142,21 @@ class CardComponent extends LitElement {
         return html`
             <article class="card-component">
                 <div class="card-component--brand-container">
-                    <span>${this.brand}</span>
-                    <figure>
+                    <span class="brand-container--brand">${this.brand}</span>
+                    <figure class="brand-container--image-container">
                         <img src=${this.img || ''} alt="product image"/>
                     </figure>
                 </div>
 
                 <div class="card-component--details-container">
-                    <h1 class="product-title">${this.productTitle}</h1>
-                    <h2 class="product-collection">${this.productCollection}</h2>
-                    <p class="product-description">${this.productDescription}</p>
-                    <div>
-                        <span class="price">${this.price}</span>
-                        <button @click=${this.#handleClick}>Comprar</button>
+                    <div class="details-container--text-info">
+                        <h1 class="text-info--product-title">${this.productTitle}</h1>
+                        <h2 class="text-info--product-collection">${this.productCollection}</h2>
+                    </div>
+                    <p class="details-container--product-description">${this.productDescription}</p>
+                    <div class="details-container--price-info">
+                        <span class="price-info--price">${this.price}</span>
+                        <button class="price-info--price-button" @click=${this.#handleClick}>Comprar</button>
                     </div>
                 </div>
             </article>
